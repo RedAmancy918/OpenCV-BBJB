3a938585346eee6d8b4a00a1d87cf2ea37b5b913
RedAmancy918 <505296809@qq.com>
2024-04-10 00:45:56 +0100

    Color detect

---
+++ Color_Detect/CMakeFiles/REDcolorMOV.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/REDcolorMOV.dir/Color_Mov.cpp.o"
+  "CMakeFiles/REDcolorMOV.dir/Color_Mov.cpp.o.d"
+  "REDcolorMOV"
+  "REDcolorMOV.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/REDcolorMOV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
